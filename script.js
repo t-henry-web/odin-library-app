be8c9fd1984d5a81e6153568f833ed884504d762
TSHenry93 <thomas-henary@homtail.co.uk>
2026-05-15 15:19:50 +0100

    fix: decouple bingo data from book constructor

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 const myLibrary = [];
 const libraryBody = document.querySelector("#library-body");
 
-function Book(title, author, pages, read, bingoCell, isHardMode = false) {
+function Book(title, author, pages, read) {
   if (!new.target) {
     throw new Error("Book must be called with the new keyword");
   }
   // Identity & Status
   this.id = crypto.randomUUID();
   this.read = read;
-
   // Metadata
   this.title = title;
   this.author = author;
   this.pages = pages;
-
-  // Bingo Specifics
-  this.bingoCell = bingoCell;
-  this.isHardMode = isHardMode;
 }
 
-function addBookToLibrary(title, author, pages, read, bingoCell, isHardMode) {
-  const newBook = new Book(title, author, pages, read, bingoCell, isHardMode);
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
@@ -39,21 +34,12 @@ function displayBooks() {
     addCell(book.title);
     addCell(book.author);
     addCell(book.pages);
-    addCell(book.bingoCell);
-    addCell(book.isHardMode ? "Hard" : "Easy");
     addCell(book.read ? "Read" : "Not Read");
 
     libraryBody.appendChild(row);
   });
 }
 
-addBookToLibrary(
-  "Pilgrim",
-  "Mitchell Luthi",
-  708,
-  false,
-  "One-Word Title",
-  true,
-); // title, author, pages, read, bingoCell, isHardMode
+addBookToLibrary("Pilgrim", "Mitchell Luthi", 708, false); // title, author, pages, read
 
 displayBooks();
