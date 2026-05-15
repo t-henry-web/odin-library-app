5c89f185314c38d9ba4dc93304b6e619d9b54327
TSHenry93 <thomas-henary@homtail.co.uk>
2026-05-15 16:05:03 +0100

    feat: disable translation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+import { bingoSquares } from "./bingo_2026.js";
+
 const myLibrary = [];
 
 const libraryBody = document.querySelector("#library-body");
-
-let bingoSquares = [];
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -28,31 +28,24 @@ function constructBingoBoard() {
   const board = document.querySelector("#bingo-board");
   board.innerHTML = "";
 
-  bingoSquares.forEach((squareData) => {
+  bingoSquares.forEach((square) => {
     const card = document.createElement("div");
     card.classList.add("bingo-card");
 
-    // Use the JSON ID as the data-id for future mapping
-    card.setAttribute("data-id", squareData.id);
+    // Use the bingo object ID as the data-id for future mapping
+    card.setAttribute("data-id", square.id);
 
     card.innerHTML = `
-      <div class="square-number">${squareData.id}</div>
-      <h3 class="square-title">${squareData.category}</h3>
-      <p class="square-desc">${squareData.description}</p>
+      <div class="square-number"></div>
+      <h3 class="square-title">${square.category}</h3>
+      <p class="square-desc">${square.description}</p>
+      <p class="square-hardmode"><strong>Hard Mode:</strong> ${square.hardModeDesc}</p>
     `;
 
     board.appendChild(card);
   });
 }
 
-async function init() {
-  const response = await fetch("./bingo_2026.json");
-  bingoSquares = await response.json();
-  constructBingoBoard();
-}
-
-init();
-
 addBookToLibrary("Pilgrim", "Mitchell Luthi", 708, false); // title, author, pages, read
 constructBingoBoard();
-displayBooks();
+// displayBooks();
